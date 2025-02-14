542c0d551230ccaeb5bc3c5d9b6c0fdd0d457817
Gustavo-Nasc <gustavo.ns@outlook.com.br>
2025-02-14 14:38:15 -0300

    feat: add fonts to theme configuration

---
+++ src/styles/font.ts
@@ -0,0 +1,13 @@
+import { Inter, JetBrains_Mono } from "next/font/google";
+
+export const primary = Inter({
+  weight: ['700', '400'],
+  variable: '--font-primary',
+  subsets: ['latin'],
+})
+
+export const secondary = JetBrains_Mono({
+  weight: ['700', '400'],
+  variable: '--font-secondary',
+  subsets: ['latin'],
+})
